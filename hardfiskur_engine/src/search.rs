62d1ad657a2d91289bcf8697616cfc1506515c8a
Tyler Zhang <t-veor@users.noreply.github.com>
2024-09-24 23:15:05 +0000

    Add quiescence search

--- hardfiskur_engine/src/search.rs
+++ hardfiskur_engine/src/search.rs
@@ -1,11 +1,15 @@
-use hardfiskur_core::board::{Board, Color, Move};
+use hardfiskur_core::{
+    board::{Board, Color, Move},
+    move_gen::{MoveGenFlags, MoveVec},
+};
 
 use crate::{evaluation::evaluate, move_ordering::order_moves, score::Score};
 
 #[derive(Debug, Default)]
 pub struct SearchStats {
-    pub nodes_searched: u64,
-    pub beta_cutoffs: u64,
+    pub nodes_searched: u32,
+    pub quiescence_nodes: u32,
+    pub beta_cutoffs: u32,
 }
 
 pub fn simple_negamax_search(
@@ -17,8 +21,6 @@ pub fn simple_negamax_search(
     stats: &mut SearchStats,
 ) -> (Score, Option<Move>) {
     stats.nodes_searched += 1;
-
-    let to_move = board.to_move();
 
     let (mut legal_moves, move_gen_result) = board.legal_moves_and_meta();
 
@@ -39,13 +41,10 @@ pub fn simple_negamax_search(
     }
 
     if depth == 0 {
-        let score = evaluate(board);
-
-        let eval = match to_move {
-            Color::White => score,
-            Color::Black => -score,
-        };
-        return (eval, None);
+        return (
+            quiescence_search(board, ply_from_root, alpha, beta, stats),
+            None,
+        );
     }
 
     order_moves(board, &mut legal_moves);
@@ -69,6 +68,54 @@ pub fn simple_negamax_search(
     }
 
     (alpha, best_move)
+}
+
+pub fn quiescence_search(
+    board: &mut Board,
+    ply_from_root: u32,
+    mut alpha: Score,
+    beta: Score,
+    stats: &mut SearchStats,
+) -> Score {
+    stats.nodes_searched += 1;
+    stats.quiescence_nodes += 1;
+
+    let mut capturing_moves = MoveVec::new();
+    board.legal_moves_ex(MoveGenFlags::GEN_CAPTURES, &mut capturing_moves);
+
+    let stand_pat_score = {
+        let score = evaluate(board);
+
+        match board.to_move() {
+            Color::White => score,
+            Color::Black => -score,
+        }
+    };
+
+    if stand_pat_score >= beta {
+        // Return beta -- opponent would not have played the previous move to
+        // get here
+        stats.beta_cutoffs += 1;
+        return beta;
+    }
+
+    alpha = alpha.max(stand_pat_score);
+
+    order_moves(board, &mut capturing_moves);
+
+    for m in capturing_moves {
+        board.push_move_unchecked(m);
+        let score = -quiescence_search(board, ply_from_root + 1, -beta, -alpha, stats);
+        board.pop_move();
+
+        if score >= beta {
+            stats.beta_cutoffs += 1;
+            return beta;
+        }
+        alpha = alpha.max(score);
+    }
+
+    return alpha;
 }
 
 pub fn simple_search(board: &mut Board) -> (Score, Option<Move>) {
