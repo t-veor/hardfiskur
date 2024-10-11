39a7689db6d4c8e8fc47bb7698dfc25964982406
Tyler Zhang <t-veor@users.noreply.github.com>
2024-10-11 15:27:52 +0000

    Basic negamax engine

--- hardfiskur_engine/src/evaluation/mod.rs
+++ hardfiskur_engine/src/evaluation/mod.rs
@@ -3,11 +3,43 @@ pub mod piece_tables;
 
 use hardfiskur_core::board::{Board, Color};
 use phase::Phase;
+use piece_tables::{material_score, piece_square_table};
 
 use crate::score::Score;
 
-pub fn evaluate_for_white_ex(_board: &Board) -> (Score, Phase) {
-    (Score(0), Phase(0))
+pub fn evaluate_for_white_ex(board: &Board) -> (Score, Phase) {
+    let mut phase = Phase(0);
+
+    let mut material = 0;
+    let mut midgame_eval = 0;
+    let mut endgame_eval = 0;
+
+    for (piece, square) in board.pieces() {
+        phase.apply_phase(piece);
+
+        let sign = match piece.color() {
+            Color::White => 1,
+            Color::Black => -1,
+        };
+
+        material += material_score(piece.piece_type());
+
+        {
+            let corrected_square = match piece.color() {
+                Color::White => square,
+                Color::Black => square.flip(),
+            };
+
+            let (mg, eg) = piece_square_table(piece.piece_type(), corrected_square);
+
+            midgame_eval += sign * mg;
+            endgame_eval += sign * eg;
+        }
+    }
+
+    let tapered_eval = phase.taper(midgame_eval, endgame_eval);
+
+    (Score(material + tapered_eval), phase)
 }
 
 pub fn evaluate_ex(board: &Board) -> (Score, Phase) {
--- hardfiskur_engine/src/search/negamax.rs
+++ hardfiskur_engine/src/search/negamax.rs
@@ -1,17 +1,81 @@
-use crate::score::Score;
+use crate::{evaluation::evaluate, score::Score};
 
 use super::SearchContext;
 
 impl<'a> SearchContext<'a> {
     pub fn negamax<const ROOT: bool>(
         &mut self,
-        _depth: i16,
-        _ply_from_root: u16,
-        _alpha: Score,
-        _beta: Score,
+        depth: i16,
+        ply_from_root: u16,
+        mut alpha: Score,
+        beta: Score,
     ) -> Score {
         self.consistency_check();
 
-        return Score(0);
+        // Increment stats
+        self.stats.nodes_searched += 1;
+        self.stats.sel_depth = self.stats.sel_depth.max(ply_from_root);
+
+        // Repetition & 50-move-rule handling
+        if self
+            .board
+            .current_position_repeated_at_least(if ply_from_root >= 2 { 1 } else { 2 })
+            || self.board.halfmove_clock() >= 100
+        {
+            return Score(0);
+        }
+
+        let (legal_moves, move_gen_result) = self.board.legal_moves_and_meta();
+
+        // Handle checkmate/stalemate
+        let in_check = move_gen_result.checker_count > 0;
+        if legal_moves.is_empty() {
+            return if in_check {
+                // Checkmate
+                -Score::mate_in_plies(ply_from_root)
+            } else {
+                // Stalemate
+                Score(0)
+            };
+        }
+
+        if depth <= 0 {
+            return evaluate(self.board);
+        }
+
+        let mut best_score = -Score::INF;
+
+        for m in legal_moves {
+            self.board.push_move_unchecked(m);
+
+            let eval = -self.negamax::<false>(depth - 1, ply_from_root + 1, -beta, -alpha);
+
+            self.board.pop_move();
+
+            // Out of time, stop searching!
+            if depth > 1 && self.should_exit_search() {
+                return best_score;
+            }
+
+            if eval > best_score {
+                best_score = eval;
+
+                if eval >= beta {
+                    // Beta cutoff!
+                    self.stats.beta_cutoffs += 1;
+                    break;
+                }
+
+                if eval > alpha {
+                    alpha = eval;
+
+                    if ROOT {
+                        self.best_root_move = Some(m);
+                    }
+                }
+            }
+        }
+
+        best_score
     }
 }
