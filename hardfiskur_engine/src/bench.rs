1c8a75432b3c016465680ababbc3799dc68e79d0
Tyler Zhang <t-veor@users.noreply.github.com>
2024-11-20 22:12:49 +0000

    Add tuned parameters with tempo bonus
    and updated default bench depth to 10
    
    Bench: 2421329

--- hardfiskur_engine/src/bench.rs
+++ hardfiskur_engine/src/bench.rs
@@ -59,7 +59,7 @@ pub const BENCH_POSITIONS: &[&str] = &[
 ];
 
 impl Engine {
-    pub const DEFAULT_BENCH_DEPTH: i16 = 9;
+    pub const DEFAULT_BENCH_DEPTH: i16 = 10;
 
     fn bench_position(&self, fen: &str, depth: i16) -> (u64, Duration) {
         self.new_game();
